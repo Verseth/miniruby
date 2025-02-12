36e45436e24698367330898eb1a233e35849a4dc
Mateusz Drewniak <matmg24@gmail.com>
2025-02-12 08:40:13 +0100

    init gem

---
+++ lib/mini_ruby.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mini_ruby/version"
+
+module MiniRuby
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/mini_ruby/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module MiniRuby
+  VERSION = "0.1.0"
+end
