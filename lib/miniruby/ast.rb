1a85527fd80ef781693fa5c5ef7955e70c84d566
Mateusz Drewniak <matmg24@gmail.com>
2025-02-18 14:08:31 +0100

    bump

--- lib/miniruby/ast.rb
+++ lib/miniruby/ast.rb
@@ -9,8 +9,8 @@ module MiniRuby
     # in S-expressions
     INDENT_UNIT = '  '
 
-    # Abstract class representing an AST node.
-    class Node
+     # Abstract class representing an AST node.
+     class Node
       extend T::Sig
       extend T::Helpers
 
@@ -153,407 +153,6 @@ module MiniRuby
       end
     end
 
-    # Represents an expression with a binary operator like `2 + 3`, `a == 4`, `5 < 2`
-    class BinaryExpressionNode < ExpressionNode
-      sig { returns(Token) }
-      attr_reader :operator
-
-      sig { returns(ExpressionNode) }
-      attr_reader :left
-
-      sig { returns(ExpressionNode) }
-      attr_reader :right
-
-      sig { params(operator: Token, left: ExpressionNode, right: ExpressionNode, span: Span).void }
-      def initialize(operator:, left:, right:, span: Span::ZERO)
-        @span = span
-        @operator = operator
-        @left = left
-        @right = right
-      end
-
-      sig { params(other: Object).returns(T::Boolean) }
-      def ==(other)
-        return false unless other.is_a?(BinaryExpressionNode)
-
-        @operator == other.operator &&
-          @left == other.left &&
-          @right == other.right
-      end
-
-      sig { override.params(indent: Integer).returns(String) }
-      def to_s(indent = 0)
-        "#{INDENT_UNIT * indent}#{@left} #{@operator} #{@right}"
-      end
-
-      sig { override.params(indent: Integer).returns(String) }
-      def inspect(indent = 0)
-        buff = String.new
-        buff << "#{INDENT_UNIT * indent}(bin_expr"
-
-        buff << "\n#{INDENT_UNIT * (indent + 1)}#{@operator}"
-        buff << "\n" << @left.inspect(indent + 1)
-        buff << "\n" << @right.inspect(indent + 1)
-
-        buff << ')'
-        buff
-      end
-    end
-
-    # Represents an identifier like `a`, `foo`
-    class IdentifierNode < ExpressionNode
-      sig { returns(String) }
-      attr_reader :value
-
-      sig { params(value: String, span: Span).void }
-      def initialize(value:, span: Span::ZERO)
-        @span = span
-        @value = value
-      end
-
-      sig { params(other: Object).returns(T::Boolean) }
-      def ==(other)
-        return false unless other.is_a?(IdentifierNode)
-
-        @value == other.value
-      end
-
-      sig { override.params(indent: Integer).returns(String) }
-      def to_s(indent = 0)
-        "#{INDENT_UNIT * indent}#{@value}"
-      end
-
-      sig { override.params(indent: Integer).returns(String) }
-      def inspect(indent = 0)
-        "#{INDENT_UNIT * indent}#{@value}"
-      end
-    end
-
-    # Represents an assignment expression like `a = 5`, `b = 2 + 5 * 5`
-    class AssignmentExpressionNode < ExpressionNode
-      sig { returns(ExpressionNode) }
-      attr_reader :target
-
-      sig { returns(ExpressionNode) }
-      attr_reader :value
-
-      sig { params(target: ExpressionNode, value: ExpressionNode, span: Span).void }
-      def initialize(target:, value:, span: Span::ZERO)
-        @span = span
-        @target = target
-        @value = value
-      end
-
-      sig { params(other: Object).returns(T::Boolean) }
-      def ==(other)
-        return false unless other.is_a?(AssignmentExpressionNode)
-
-        @target == other.target &&
-          @value == other.value
-      end
-
-      sig { override.params(indent: Integer).returns(String) }
-      def to_s(indent = 0)
-        "#{INDENT_UNIT * indent}#{@target} = #{@value}"
-      end
-
-      sig { override.params(indent: Integer).returns(String) }
-      def inspect(indent = 0)
-        buff = String.new
-        buff << "#{INDENT_UNIT * indent}(assignment"
-
-        buff << "\n" << @target.inspect(indent + 1)
-        buff << "\n" << @value.inspect(indent + 1)
-
-        buff << ')'
-        buff
-      end
-    end
-
-    # Represents an expression with a unary operator like `+3`, `-a`
-    class UnaryExpressionNode < ExpressionNode
-      sig { returns(Token) }
-      attr_reader :operator
-
-      sig { returns(ExpressionNode) }
-      attr_reader :value
-
-      sig { params(operator: Token, value: ExpressionNode, span: Span).void }
-      def initialize(operator:, value:, span: Span::ZERO)
-        @span = span
-        @operator = operator
-        @value = value
-      end
-
-      sig { params(other: Object).returns(T::Boolean) }
-      def ==(other)
-        return false unless other.is_a?(UnaryExpressionNode)
-
-        @operator == other.operator &&
-          @value == other.value
-      end
-
-      sig { override.params(indent: Integer).returns(String) }
-      def to_s(indent = 0)
-        "#{INDENT_UNIT * indent}#{@operator}#{@value}"
-      end
-
-      sig { override.params(indent: Integer).returns(String) }
-      def inspect(indent = 0)
-        buff = String.new
-        buff << "#{INDENT_UNIT * indent}(unary_expr"
-
-        buff << "\n" << @operator.to_s
-        buff << "\n" << @value.inspect(indent + 1)
-
-        buff << ')'
-        buff
-      end
-    end
-
-    # Represents a return like `return 3`, `return 1 + 5 * a`
-    class ReturnExpressionNode < ExpressionNode
-      sig { returns(T.nilable(ExpressionNode)) }
-      attr_reader :value
-
-      sig { params(value: T.nilable(ExpressionNode), span: Span).void }
-      def initialize(value: nil, span: Span::ZERO)
-        @span = span
-        @value = value
-      end
-
-      sig { params(other: Object).returns(T::Boolean) }
-      def ==(other)
-        return false unless other.is_a?(ReturnExpressionNode)
-
-        @value == other.value
-      end
-
-      sig { override.params(indent: Integer).returns(String) }
-      def to_s(indent = 0)
-        buff = String.new
-        buff << "#{INDENT_UNIT * indent}return"
-        buff << " #{@value}" if @value
-
-        buff
-      end
-
-      sig { override.params(indent: Integer).returns(String) }
-      def inspect(indent = 0)
-        buff = String.new
-        buff << "#{INDENT_UNIT * indent}(return"
-        buff << " #{@value.inspect}" if @value
-        buff << ')'
-
-        buff
-      end
-    end
-
-    # Represents an if expression like `if foo; a = 5; end`
-    class IfExpressionNode < ExpressionNode
-      sig { returns(ExpressionNode) }
-      attr_reader :condition
-
-      sig { returns(T::Array[StatementNode]) }
-      attr_reader :then_body
-
-      sig { returns(T.nilable(T::Array[StatementNode])) }
-      attr_reader :else_body
-
-      sig do
-        params(
-          condition: ExpressionNode,
-          then_body: T::Array[StatementNode],
-          else_body: T.nilable(T::Array[StatementNode]),
-          span:      Span,
-        ).void
-      end
-      def initialize(condition:, then_body:, else_body: nil, span: Span::ZERO)
-        @span = span
-        @condition = condition
-        @then_body = then_body
-        @else_body = else_body
-      end
-
-      sig { params(other: Object).returns(T::Boolean) }
-      def ==(other)
-        return false unless other.is_a?(IfExpressionNode)
-
-        @condition == other.condition &&
-          @then_body == other.then_body &&
-          @else_body == other.else_body
-      end
-
-      sig { override.params(indent: Integer).returns(String) }
-      def to_s(indent = 0)
-        buff = String.new
-        indent_str = INDENT_UNIT * indent
-        buff << "#{indent_str}if #{@condition}\n"
-
-        @then_body.each do |stmt|
-          buff << stmt.to_s(indent + 1)
-        end
-
-        els = @else_body
-        if els
-          buff << "#{indent_str}else\n"
-          els.each do |stmt|
-            buff << stmt.to_s(indent + 1)
-          end
-        end
-        buff << "#{indent_str}end"
-
-        buff
-      end
-
-      sig { override.params(indent: Integer).returns(String) }
-      def inspect(indent = 0)
-        buff = String.new
-        buff << "#{INDENT_UNIT * indent}(if"
-
-        buff << "\n" << @condition.inspect(indent + 1)
-
-        buff << "\n#{INDENT_UNIT * (indent + 1)}(then"
-        @then_body.each do |stmt|
-          buff << "\n" << stmt.inspect(indent + 2)
-        end
-        buff << ')'
-
-        els = @else_body
-        if els
-          buff << "\n#{INDENT_UNIT * (indent + 1)}(else"
-          els.each do |stmt|
-            buff << "\n" << stmt.inspect(indent + 2)
-          end
-          buff << ')'
-        end
-
-        buff << ')'
-        buff
-      end
-    end
-
-    # Represents a while expression like `while foo; a = 5; end`
-    class WhileExpressionNode < ExpressionNode
-      sig { returns(ExpressionNode) }
-      attr_reader :condition
-
-      sig { returns(T::Array[StatementNode]) }
-      attr_reader :then_body
-
-      sig do
-        params(
-          condition: ExpressionNode,
-          then_body: T::Array[StatementNode],
-          span:      Span,
-        ).void
-      end
-      def initialize(condition:, then_body:, span: Span::ZERO)
-        @span = span
-        @condition = condition
-        @then_body = then_body
-      end
-
-      sig { params(other: Object).returns(T::Boolean) }
-      def ==(other)
-        return false unless other.is_a?(WhileExpressionNode)
-
-        @condition == other.condition &&
-          @then_body == other.then_body
-      end
-
-      sig { override.params(indent: Integer).returns(String) }
-      def to_s(indent = 0)
-        buff = String.new
-        indent_str = INDENT_UNIT * indent
-        buff << "#{indent_str}while #{@condition}\n"
-
-        @then_body.each do |stmt|
-          buff << stmt.to_s(indent + 1)
-        end
-
-        buff << "#{indent_str}end"
-
-        buff
-      end
-
-      sig { override.params(indent: Integer).returns(String) }
-      def inspect(indent = 0)
-        buff = String.new
-        buff << "#{INDENT_UNIT * indent}(while"
-
-        buff << "\n" << @condition.inspect(indent + 1)
-
-        buff << "#{INDENT_UNIT * (indent + 1)}(then"
-        @then_body.each do |stmt|
-          buff << "\n" << stmt.inspect(indent + 2)
-        end
-        buff << ')'
-
-        buff << ')'
-        buff
-      end
-    end
-
-    # Represents a function call like `foo(1, bar)`
-    class FunctionCallNode < ExpressionNode
-      sig { returns(String) }
-      attr_reader :name
-
-      sig { returns(T::Array[ExpressionNode]) }
-      attr_reader :arguments
-
-      sig do
-        params(
-          name:      String,
-          arguments: T::Array[ExpressionNode],
-          span:      Span,
-        ).void
-      end
-      def initialize(name:, arguments: [], span: Span::ZERO)
-        @span = span
-        @name = name
-        @arguments = arguments
-      end
-
-      sig { params(other: Object).returns(T::Boolean) }
-      def ==(other)
-        return false unless other.is_a?(FunctionCallNode)
-
-        @name == other.name &&
-          @arguments == other.arguments
-      end
-
-      sig { override.params(indent: Integer).returns(String) }
-      def to_s(indent = 0)
-        buff = String.new
-        indent_str = INDENT_UNIT * indent
-
-        buff << "#{indent_str}#{@name}("
-        @arguments.each.with_index do |arg, i|
-          buff << ', ' if i > 0
-          buff << arg.to_s
-        end
-        buff << ')'
-
-        buff
-      end
-
-      sig { override.params(indent: Integer).returns(String) }
-      def inspect(indent = 0)
-        buff = String.new
-        buff << "#{INDENT_UNIT * indent}(call\n"
-
-        buff << "#{INDENT_UNIT * (indent + 1)}#{@name}"
-        @arguments.each do |arg|
-          buff << "\n" << arg.inspect(indent + 1)
-        end
-        buff << ')'
-
-        buff
-      end
-    end
-
     # Represents a false literal eg. `false`
     class FalseLiteralNode < ExpressionNode
       sig { override.params(indent: Integer).returns(String) }
@@ -690,6 +289,35 @@ module MiniRuby
       sig { override.params(indent: Integer).returns(String) }
       def inspect(indent = 0)
         "#{INDENT_UNIT * indent}#{value.inspect}"
+      end
+    end
+
+    # Represents an identifier like `a`, `foo`
+    class IdentifierNode < ExpressionNode
+      sig { returns(String) }
+      attr_reader :value
+
+      sig { params(value: String, span: Span).void }
+      def initialize(value:, span: Span::ZERO)
+        @span = span
+        @value = value
+      end
+
+      sig { params(other: Object).returns(T::Boolean) }
+      def ==(other)
+        return false unless other.is_a?(IdentifierNode)
+
+        @value == other.value
+      end
+
+      sig { override.params(indent: Integer).returns(String) }
+      def to_s(indent = 0)
+        "#{INDENT_UNIT * indent}#{@value}"
+      end
+
+      sig { override.params(indent: Integer).returns(String) }
+      def inspect(indent = 0)
+        "#{INDENT_UNIT * indent}#{@value}"
       end
     end
 
--- lib/miniruby/parser.rb
+++ lib/miniruby/parser.rb
@@ -50,18 +50,18 @@ module MiniRuby
       AST::ProgramNode.new(statements:, span:)
     end
 
-    # statements = statement*
-    sig { params(stop_tokens: Symbol).returns(T::Array[AST::StatementNode]) }
-    def parse_statements(*stop_tokens)
-      statements = T.let([], T::Array[AST::StatementNode])
-      swallow_statement_separators
+     # statements = statement*
+     sig { params(stop_tokens: Symbol).returns(T::Array[AST::StatementNode]) }
+     def parse_statements(*stop_tokens)
+       statements = T.let([], T::Array[AST::StatementNode])
+       swallow_statement_separators
 
-      while true
-        return statements if accept!([Token::END_OF_FILE, *stop_tokens])
+       while true
+         return statements if accept!([Token::END_OF_FILE, *stop_tokens])
 
-        statements << parse_statement
-      end
-    end
+         statements << parse_statement
+       end
+     end
 
     # statement = expression_statement
     sig { returns(AST::StatementNode) }
@@ -84,175 +84,8 @@ module MiniRuby
       AST::ExpressionStatementNode.new(expression:, span:)
     end
 
-    # expression = assignment_expression
     sig { returns(AST::ExpressionNode) }
     def parse_expression
-      parse_assignment_expression
-    end
-
-    # assignment_expression = expression "=" assignment_expression | equality_expression
-    sig { returns(AST::ExpressionNode) }
-    def parse_assignment_expression
-      target = parse_equality_expression
-      return target unless match(Token::EQUAL)
-
-      unless target.is_a?(AST::IdentifierNode)
-        add_error("unexpected `#{target.class}`, expected an identifier")
-      end
-      swallow_newlines
-      value = parse_assignment_expression
-      span = target.span.join(value.span)
-
-      AST::AssignmentExpressionNode.new(span:, target:, value:)
-    end
-
-    # equality_expression = equality_expression ("==" | "!=") comparison_expression | comparison_expression
-    sig { returns(AST::ExpressionNode) }
-    def parse_equality_expression
-      left = parse_comparison_expression
-
-      while @lookahead.equality_operator?
-        operator = advance
-        swallow_newlines
-
-        right = parse_comparison_expression
-        span = left.span.join(right.span)
-        left = AST::BinaryExpressionNode.new(
-          span:,
-          operator:,
-          left:,
-          right:,
-        )
-      end
-
-      left
-    end
-
-    # comparison_expression = comparison_expression (">" | ">=" | "<" | "<=") additive_expression | additive_expression
-    sig { returns(AST::ExpressionNode) }
-    def parse_comparison_expression
-      left = parse_additive_expression
-
-      while @lookahead.comparison_operator?
-        operator = advance
-        swallow_newlines
-
-        right = parse_additive_expression
-        span = left.span.join(right.span)
-        left = AST::BinaryExpressionNode.new(
-          span:,
-          operator:,
-          left:,
-          right:,
-        )
-      end
-
-      left
-    end
-
-    # additive_expression = additive_expression ("+" | "-") multiplicative_expression | multiplicative_expression
-    sig { returns(AST::ExpressionNode) }
-    def parse_additive_expression
-      left = parse_multiplicative_expression
-
-      while @lookahead.additive_operator?
-        operator = advance
-        swallow_newlines
-
-        right = parse_multiplicative_expression
-        span = left.span.join(right.span)
-        left = AST::BinaryExpressionNode.new(
-          span:,
-          operator:,
-          left:,
-          right:,
-        )
-      end
-
-      left
-    end
-
-    # multiplicative_expression = multiplicative_expression ("*" | "/") unary_expression | unary_expression
-    sig { returns(AST::ExpressionNode) }
-    def parse_multiplicative_expression
-      left = parse_unary_expression
-
-      while @lookahead.multiplicative_operator?
-        operator = advance
-        swallow_newlines
-
-        right = parse_unary_expression
-        span = left.span.join(right.span)
-        left = AST::BinaryExpressionNode.new(
-          span:,
-          operator:,
-          left:,
-          right:,
-        )
-      end
-
-      left
-    end
-
-    # unary_expression = function_call | ("!" | "-" | "+") unary_expression
-    sig { returns(AST::ExpressionNode) }
-    def parse_unary_expression
-      if (operator = match(Token::BANG, Token::MINUS, Token::PLUS))
-        swallow_newlines
-        value = parse_unary_expression
-        span = operator.span.join(value.span)
-
-        return AST::UnaryExpressionNode.new(
-          span:,
-          operator:,
-          value:,
-        )
-      end
-
-      parse_function_call
-    end
-
-    # function_call = IDENTIFIER "(" argument_list ")" | primary_expression
-    sig { returns(AST::ExpressionNode) }
-    def parse_function_call
-      ident = parse_primary_expression
-      return ident unless ident.is_a?(AST::IdentifierNode) && match(Token::LPAREN)
-
-      swallow_newlines
-      arg_list = parse_argument_list
-      swallow_newlines
-
-      rparen, ok = consume(Token::RPAREN)
-      return AST::InvalidNode.new(span: rparen.span, token: rparen) unless ok
-
-      span = ident.span.join(rparen.span)
-      AST::FunctionCallNode.new(
-        span:,
-        name:      ident.value,
-        arguments: arg_list,
-      )
-    end
-
-    sig { returns(T::Array[AST::ExpressionNode]) }
-    def parse_argument_list
-      return [] if accept(Token::RPAREN)
-
-      args = [parse_expression]
-      while true
-        break if accept(Token::END_OF_FILE, Token::RPAREN)
-        break unless match(Token::COMMA)
-
-        swallow_newlines
-        break if accept(Token::END_OF_FILE, Token::RPAREN)
-
-        args << parse_expression
-      end
-
-      args
-    end
-
-    sig { returns(AST::ExpressionNode) }
-    def parse_primary_expression
       case @lookahead.type
       when Token::FALSE
         tok = advance
@@ -263,9 +96,6 @@ module MiniRuby
       when Token::NIL
         tok = advance
         AST::NilLiteralNode.new(span: tok.span)
-      when Token::SELF
-        tok = advance
-        AST::SelfLiteralNode.new(span: tok.span)
       when Token::INTEGER
         tok = advance
         AST::IntegerLiteralNode.new(span: tok.span, value: T.must(tok.value))
@@ -278,99 +108,11 @@ module MiniRuby
       when Token::IDENTIFIER
         tok = advance
         AST::IdentifierNode.new(span: tok.span, value: T.must(tok.value))
-      when Token::RETURN
-        parse_return_expression
-      when Token::IF
-        parse_if_expression
-      when Token::WHILE
-        parse_while_expression
-      when Token::LPAREN
-        parse_parenthesized_expression
       else
         token = advance
         add_error("unexpected token `#{tok}`") if token.type != Token::ERROR
         AST::InvalidNode.new(span: token.span, token:)
       end
-    end
-
-    # parenthesized_expression = "(" expression ")"
-    sig { returns AST::ExpressionNode }
-    def parse_parenthesized_expression
-      lparen = advance
-      expr = parse_expression
-      rparen, = consume(Token::RPAREN)
-      expr.span = lparen.span.join(rparen.span)
-
-      expr
-    end
-
-    # return_expression = "return" [expression]
-    sig { returns(AST::ReturnExpressionNode) }
-    def parse_return_expression
-      return_token = advance
-      if accept(Token::END_OF_FILE, Token::NEWLINE, Token::SEMICOLON)
-        return AST::ReturnExpressionNode.new(span: return_token.span)
-      end
-
-      value = parse_expression
-      span = return_token.span.join(value.span)
-
-      AST::ReturnExpressionNode.new(span:, value:)
-    end
-
-    # if_expression = "if" expression SEPARATOR statements ["else" (expression | SEPARATOR statements)] "end"
-    sig { returns(AST::ExpressionNode) }
-    def parse_if_expression
-      if_token = advance
-      condition = parse_expression
-
-      separator, ok = consume(Token::SEMICOLON, Token::NEWLINE)
-      return AST::InvalidNode.new(span: separator.span, token: separator) unless ok
-
-      then_body = parse_statements(Token::END_K, Token::ELSE)
-      else_body = T.let(nil, T.nilable(T::Array[MiniRuby::AST::StatementNode]))
-      span = if_token.span
-
-      if match(Token::ELSE)
-        if match(Token::NEWLINE, Token::SEMICOLON)
-          # else; a + 5; end
-          else_body = parse_statements(Token::END_K)
-          end_tok, ok = consume(Token::END_K)
-          return AST::InvalidNode.new(span: end_tok.span, token: end_tok) unless ok
-
-          span = span.join(end_tok.span)
-        else
-          # else a + 5
-          expression = parse_expression
-          else_body = [AST::ExpressionStatementNode.new(span: expression.span, expression:)]
-          span = span.join(expression.span)
-        end
-      else
-        end_tok, ok = consume(Token::END_K)
-        return AST::InvalidNode.new(span: end_tok.span, token: end_tok) unless ok
-
-        span = span.join(end_tok.span)
-      end
-
-      AST::IfExpressionNode.new(span:, condition:, then_body:, else_body:)
-    end
-
-    # if_expression = "while" expression SEPARATOR statements "end"
-    sig { returns(AST::ExpressionNode) }
-    def parse_while_expression
-      while_token = advance
-      condition = parse_expression
-
-      separator, ok = consume(Token::SEMICOLON, Token::NEWLINE)
-      return AST::InvalidNode.new(span: separator.span, token: separator) unless ok
-
-      then_body = parse_statements(Token::END_K)
-
-      end_tok, ok = consume(Token::END_K)
-      return AST::InvalidNode.new(span: end_tok.span, token: end_tok) unless ok
-
-      span = while_token.span.join(end_tok.span)
-      AST::WhileExpressionNode.new(span:, condition:, then_body:)
     end
 
     # Move over to the next token.
@@ -398,17 +140,6 @@ module MiniRuby
       @errors << err
     end
 
-    # Checks if the next token matches any of the given types,
-    # if so it gets consumed.
-    sig { params(token_types: Symbol).returns(T.nilable(Token)) }
-    def match(*token_types)
-      token_types.each do |type|
-        return advance if accept(type)
-      end
-
-      nil
-    end
-
     # Checks whether the next token matches any the specified types.
     sig { params(token_types: Symbol).returns(T::Boolean) }
     def accept(*token_types)
@@ -425,26 +156,15 @@ module MiniRuby
       false
     end
 
-    sig { params(token_types: Symbol).returns([Token, T::Boolean]) }
-    def consume(*token_types)
-      return advance, false if @lookahead.type == Token::ERROR
-
-      if token_types.any? { _1 == @lookahead.type }
-        return advance, true
+    # Checks if the next token matches any of the given types,
+    # if so it gets consumed.
+    sig { params(token_types: Symbol).returns(T.nilable(Token)) }
+    def match(*token_types)
+      token_types.each do |type|
+        return advance if accept(type)
       end
 
-      msg = token_types.map { Token.type_to_string(_1) }.join(' or ')
-      error_expected(msg)
-      [advance, false]
-    end
-
-    # Adds an error which tells the user that another type of token
-    # was expected.
-    sig { params(expected: String).void }
-    def error_expected(expected)
-      return if @lookahead.type == Token::ERROR
-
-      add_error("unexpected #{@lookahead.type_name}, expected #{expected}")
+      nil
     end
 
     # Accept and ignore any number of consecutive newline tokens.
@@ -463,5 +183,16 @@ module MiniRuby
       end
     end
 
+    # Adds an error which tells the user that another type of token
+    # was expected.
+    sig { params(expected: String).void }
+    def error_expected(expected)
+      return if @lookahead.type == Token::ERROR
+
+      add_error("unexpected #{@lookahead.type_name}, expected #{expected}")
+    end
+
+
   end
+
 end
